fb3550ec228a3a24e07b07496ca4bc39afd9fc8f
Adhitya Balamurugan <aadhityabalamurugan@gmail.com>
2017-07-07 04:06:51 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+from cStringIO import StringIO
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+import re
+
+def convert(fname):
+    pages=None
+    if not pages:
+        pagenums = set()
+    else:
+        pagenums = set(pages)
+
+    output = StringIO()
+    manager = PDFResourceManager()
+    converter = TextConverter(manager, output, laparams=LAParams())
+    interpreter = PDFPageInterpreter(manager, converter)
+
+    infile = file(fname, 'r')
+    for page in PDFPage.get_pages(infile, pagenums):
+        interpreter.process_page(page)
+    infile.close()
+    converter.close()
+    text = output.getvalue()
+    output.close
+    print text 
+
+    # write Content to .txt
+    text_file = open("Output_1.txt", "w")
+    text = re.sub("\s\s+", " ", text)
+    text_file.write("%s" % text)
+    text_file.close()
+
+convert("File.pdf")
